4382ecee0300e1f512f8651478fb67d530c091fb
molly mk <mmkover@gmail.com>
2020-08-22 14:06:01 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,8 +2,7 @@ def my_collect(array)
   i = 0 
   language = []
   while i < array.length 
-  capitals = array.upcase
-  language << yield(capitals[i])
+  language << yield(array[i])
   i += 1 
 end
 language
